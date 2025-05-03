7b415771d11ea88d621850d674eb22857e2ef4a5
H2Sxxa <h2sxxa0w0@gmail.com>
2025-05-03 12:44:56 +0800

    ✨ PistonMetaData

--- src/model/mojang.rs
+++ src/model/mojang.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use serde::Deserialize;
 
 pub struct MojangBaseUrl {
@@ -41,6 +43,77 @@ pub struct LaunchMetaVersionData {
 }
 
 /// https://piston-meta.mojang.com/v1/packages/<->/<->.json
+#[derive(Debug, Deserialize)]
 pub struct PistonMetaData {
-    //TODO
+    pub arguments: PistonMetaArguments,
+    #[serde(rename = "assetIndex")]
+    pub asset_index: PistonMetaAssetIndex,
+    pub assets: String,
+    #[serde(rename = "complianceLevel")]
+    pub compliance_level: usize,
+    pub downloads: PistonMetaDownloads,
+    pub id: String,
+    #[serde(rename = "javaVersion")]
+    pub java_version: PistonMetaJavaVersion,
+    //TODO pub libraries: ...
+}
+
+#[derive(Debug, Deserialize)]
+pub struct PistonMetaArguments {
+    pub game: Vec<PistonMetaGenericArgument>,
+    pub jvm: Vec<PistonMetaGenericArgument>,
+}
+#[derive(Debug, Deserialize)]
+pub enum PistonMetaGenericArgument {
+    Plain(String),
+    Rule(PistonMetaRuleArgument),
+}
+
+#[derive(Debug, Deserialize)]
+pub struct PistonMetaRuleArgument {
+    pub rules: Vec<PistonMetaRuleArgumentRules>,
+    pub value: ContinuousArgument,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct PistonMetaRuleArgumentRules {
+    pub action: String,
+    pub features: Option<HashMap<String, String>>,
+    pub os: Option<HashMap<String, String>>,
+}
+
+#[derive(Debug, Deserialize)]
+pub enum ContinuousArgument {
+    Single(String),
+    Multi(Vec<String>),
+}
+
+#[derive(Debug, Deserialize)]
+pub struct PistonMetaAssetIndex {
+    pub id: String,
+    pub sha1: String,
+    // size may be too small
+    pub size: usize,
+    #[serde(rename = "totalSize")]
+    pub total_size: usize,
+    pub url: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct PistonMetaDownloads {
+    pub client: PistonMetaDownload,
+    pub server: PistonMetaDownload,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct PistonMetaDownload {
+    pub sha1: String,
+    pub size: usize,
+    pub url: String,
+}
+#[derive(Debug, Deserialize)]
+pub struct PistonMetaJavaVersion {
+    pub component: String,
+    #[serde(rename = "majorVersion")]
+    pub major_version: usize,
 }

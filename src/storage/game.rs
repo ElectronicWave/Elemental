508938fdc77e1cb093edbd9cc037509c37728664
H2Sxxa <h2sxxa0w0@gmail.com>
2025-05-12 21:53:40 +0800

    💩 Make Code More Beautiful

--- src/storage/game.rs
+++ src/storage/game.rs
@@ -22,24 +22,20 @@ impl GameStorage {
 
     pub fn new_ensure_dir(root: impl Into<String>) -> Result<Self> {
         let root = root.into();
-        if let Err(err) = create_dir_all(&root) {
-            Err(err)
-        } else {
-            Ok(Self { root })
-        }
+        create_dir_all(&root)?;
+
+        Ok(Self { root })
     }
 
     pub fn get_ensure_object_indexes_path(&self, version_id: String) -> Result<String> {
         let parent = self.join("assets").join("indexes");
 
-        if let Err(err) = create_dir_all(parent.clone()) {
-            Err(err)
-        } else {
-            Ok(parent
-                .join(format!("{version_id}.json"))
-                .to_string_lossy()
-                .to_string())
-        }
+        create_dir_all(&parent)?;
+
+        Ok(parent
+            .join(format!("{version_id}.json"))
+            .to_string_lossy()
+            .to_string())
     }
 
     pub async fn get_and_save_objects_index(
@@ -67,11 +63,9 @@ impl GameStorage {
             .join("objects")
             .join(hash.get(0..2).unwrap());
 
-        if let Err(err) = create_dir_all(parent.clone()) {
-            Err(err)
-        } else {
-            Ok(parent.join(hash).to_string_lossy().to_string())
-        }
+        create_dir_all(&parent)?;
+
+        Ok(parent.join(hash).to_string_lossy().to_string())
     }
 
     pub fn get_natives_path(&self) -> String {
@@ -83,17 +77,12 @@ impl GameStorage {
         library: PistonMetaLibrariesDownloadsArtifact,
     ) -> Result<String> {
         let path = self.join("libraries").join(&library.path);
-        let path_parent = path.parent();
+        let parent = path
+            .parent()
+            .ok_or(Error::new(ErrorKind::Other, "No such directory"))?;
 
-        if let None = path_parent {
-            return Err(Error::new(ErrorKind::Other, "No such directory"));
-        }
-
-        if let Err(err) = create_dir_all(path_parent.unwrap()) {
-            Err(err)
-        } else {
-            Ok(path.to_string_lossy().to_string())
-        }
+        create_dir_all(parent)?;
+        Ok(path.to_string_lossy().to_string())
     }
 
     pub fn join<P: AsRef<Path>>(&self, path: P) -> PathBuf {

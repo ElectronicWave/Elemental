c794307cb05e94582119109d6850550f15ef3cf6
H2Sxxa <h2sxxa0w0@gmail.com>
2025-05-16 14:28:25 +0800

    ✨ Complete JDK release file parse

--- src/bootstrap/java.rs
+++ src/bootstrap/java.rs
@@ -1,13 +1,51 @@
-use std::env::var;
+use std::{
+    collections::HashMap, env::var, fs::read_to_string, hash::RandomState, io::Result, path::Path,
+};
 
 #[derive(Debug)]
 pub struct JavaDistrubtion {
     pub path: String,
-    pub info: Option<JavaDistrubtionReleaseInfo>,
+    pub info: Result<JavaDistrubtionReleaseInfo>,
 }
 
 #[derive(Debug)]
-pub struct JavaDistrubtionReleaseInfo {}
+pub struct JavaDistrubtionReleaseInfo {
+    pub implememtor: String,
+    pub implememtor_version: String,
+    pub java_runtime_version: String,
+}
+impl JavaDistrubtionReleaseInfo {
+    pub fn parse_from_string(source: String) -> Self {
+        let data: HashMap<&str, &str, RandomState> =
+            HashMap::from_iter(source.lines().filter_map(|e| {
+                if let Some((k, v)) = e.split_once("=") {
+                    return Some((k, v.trim_start_matches('"').trim_end_matches('"')));
+                }
+                None
+            }));
+
+        Self {
+            implememtor: data
+                .get("IMPLEMENTOR")
+                .unwrap_or(&"IMPLEMEMTOR")
+                .to_string(),
+            implememtor_version: data
+                .get("IMPLEMENTOR_VERSION")
+                .unwrap_or(&"IMPLEMENTOR_VERSION")
+                .to_string(),
+            java_runtime_version: data
+                .get("JAVA_RUNTIME_VERSION")
+                .unwrap_or(&"JAVA_RUNTIME_VERSION")
+                .to_string(),
+        }
+    }
+
+    pub fn parse_from_file<P: AsRef<Path>>(path: P) -> Result<Self> {
+        Ok(JavaDistrubtionReleaseInfo::parse_from_string(
+            read_to_string(path)?,
+        ))
+    }
+}
 
 impl JavaDistrubtion {
     pub fn get_all_java_distrubtion() -> Vec<Self> {
@@ -21,7 +59,11 @@ impl JavaDistrubtion {
     pub fn get_javahome_java_distrubtion() -> Option<Self> {
         let javahome = var("JAVA_HOME").ok();
         if let Some(path) = javahome {
-            return Some(JavaDistrubtion { path, info: None });
+            let releasefile = Path::new(&path.clone()).join("release");
+            return Some(JavaDistrubtion {
+                path,
+                info: JavaDistrubtionReleaseInfo::parse_from_file(releasefile),
+            });
         }
 
         None

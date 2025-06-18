5c19ba5b43bbf916cb8592046b292cccbdbfcdc5
H2Sxxa <h2sxxa0w0@gmail.com>
2025-06-18 22:20:02 +0800

    ✨ Complete Downloader

--- crates/core/src/online/downloader.rs
+++ crates/core/src/online/downloader.rs
@@ -3,15 +3,48 @@ use dashmap::{
     mapref::one::{Ref, RefMut},
 };
 use futures::StreamExt;
-use std::{io::Result, sync::LazyLock};
+use std::{
+    io::Result,
+    sync::{Arc, LazyLock},
+};
 use tokio::task::JoinSet;
 
 use crate::error::unification::UnifiedResult;
 
 pub struct ElementalDownloader {
     client: reqwest::Client,
-    handler: DashMap<String, JoinSet<()>>,
+    handler: DashMap<String, JoinSet<()>>, // Group name : JoinSet()
+    pub tracker: Arc<ElementalTaskTracker>,
 }
+
+pub struct ElementalTaskTracker {
+    pub tasks: DashMap<String, DashMap<DownloadTask, TrackedInfo>>,
+}
+
+impl ElementalTaskTracker {
+    pub fn new() -> Self {
+        Self {
+            tasks: DashMap::new(),
+        }
+    }
+
+    pub fn track_task(&self, task: &DownloadTask) {
+        self.tasks
+            .get_mut(&task.group)
+            .map(|mut tasks| tasks.value_mut().insert(task.clone(), task.track()));
+    }
+
+    pub fn create_track_group(&self, group: impl Into<String>) {
+        self.tasks.insert(group.into(), DashMap::new());
+    }
+    pub fn remove_track_group(&self, group: impl Into<String>) {
+        self.tasks.remove(&group.into());
+    }
+    pub fn has_track_group(&self, group: impl Into<String>) -> bool {
+        self.tasks.contains_key(&group.into())
+    }
+}
+
 static SHARED_DOWNLOADER: LazyLock<ElementalDownloader> = LazyLock::new(ElementalDownloader::new);
 
 impl ElementalDownloader {
@@ -19,6 +52,7 @@ impl ElementalDownloader {
         Self {
             client: reqwest::Client::new(),
             handler: DashMap::new(),
+            tracker: Arc::new(ElementalTaskTracker::new()),
         }
     }
 
@@ -27,14 +61,22 @@ impl ElementalDownloader {
     }
 
     pub fn create_task_group(&self, group: impl Into<String>) -> Option<JoinSet<()>> {
-        self.handler.insert(group.into(), JoinSet::new())
+        let group = group.into();
+        self.tracker.create_track_group(&group);
+        self.handler.insert(group, JoinSet::new())
     }
 
     pub fn remove_task_group(&self, group: impl Into<String>) {
-        self.handler.remove(&group.into()).map(|(_, mut handler)| {
+        let group = group.into();
+        self.tracker.remove_track_group(&group);
+        self.handler.remove(&group).map(|(_, mut handler)| {
             handler.abort_all();
             drop(handler)
         });
+    }
+
+    pub fn has_task_group(&self, group: impl Into<String>) -> bool {
+        self.handler.contains_key(&group.into())
     }
 
     pub fn get_task_group(&self, group: impl Into<String>) -> Option<Ref<'_, String, JoinSet<()>>> {
@@ -52,7 +94,15 @@ impl ElementalDownloader {
         let client = self.client.clone();
         let url = task.url.clone();
         let path = task.path.clone();
+        let group = task.group.clone();
+        let tracker = self.tracker.clone();
+
         self.handler.get_mut(&task.group).map(|mut handler| {
+            tracker.track_task(&task);
+            let task_cloned = task.clone();
+            let group_cloned = group.clone();
+            let tracker_cloned = tracker.clone();
+
             handler.value_mut().spawn(async move {
                 let executer: Result<()> = async move {
                     let mut stream = client
@@ -62,10 +112,18 @@ impl ElementalDownloader {
                         .to_stdio()?
                         .bytes_stream();
                     let mut output = tokio::fs::File::create(path).await?;
-                    let mut size = 0; // TODO Trace progress here
                     while let Some(item) = stream.next().await {
                         let data = item.to_stdio()?;
-                        size += data.len();
+
+                        tracker_cloned
+                            .tasks
+                            .get_mut(&group_cloned)
+                            .map(|mut tasks| {
+                                tasks.value_mut().get_mut(&task.clone()).map(|mut tracked| {
+                                    tracked.value_mut().recv += data.len();
+                                })
+                            });
+
                         tokio::io::copy(&mut data.as_ref(), &mut output).await?;
                     }
                     Ok(())
@@ -73,9 +131,17 @@ impl ElementalDownloader {
                 .await;
 
                 match executer {
-                    Ok(_) => (),
-                    Err(error) => (), // TODO Trace error here
-                }
+                    Ok(_) => tracker.tasks.get_mut(&group).map(|mut tasks| {
+                        tasks.value_mut().get_mut(&task_cloned).map(|mut tracked| {
+                            tracked.value_mut().status = TrackedTaskStatus::DONE;
+                        })
+                    }),
+                    Err(error) => tracker.tasks.get_mut(&group).map(|mut tasks| {
+                        tasks.value_mut().get_mut(&task_cloned).map(|mut tracked| {
+                            tracked.value_mut().status = TrackedTaskStatus::ERR(error.to_string());
+                        })
+                    }),
+                };
             });
         })
     }
@@ -92,6 +158,19 @@ pub struct DownloadTask {
     pub group: String,
 }
 
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+pub struct TrackedInfo {
+    pub recv: usize,
+    pub status: TrackedTaskStatus,
+}
+
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+pub enum TrackedTaskStatus {
+    ERR(String),
+    ACTIVE,
+    DONE,
+}
+
 impl DownloadTask {
     // usually use `version_name` as task_group_name.
     pub fn new(url: impl Into<String>, path: impl Into<String>, group: impl Into<String>) -> Self {
@@ -99,6 +178,13 @@ impl DownloadTask {
             url: url.into(),
             path: path.into(),
             group: group.into(),
+        }
+    }
+
+    pub fn track(&self) -> TrackedInfo {
+        TrackedInfo {
+            recv: 0,
+            status: TrackedTaskStatus::ACTIVE,
         }
     }
 }

16dd46cf30e804ab7e304da952df67a7772f615c
oganesson897 <oganesson897@gmail.com>
2026-01-01 10:11:33 +0900

    feat: removed discover

--- crates/discover/src/main.rs
+++
@@ -1,8 +0,0 @@
-use curl::easy::{Easy};
-use elemental_discover::curseforge::Curse;
-
-fn main() {
-    let curse = Curse::new_with_default_key(false);
-    //curse.get_mod(238222);
-    println!("{:?}", curse.get_mod(238222))
-}
